b38b2e261cb28ed6b7ac6f914f7821b1bf2af9cc
subin <subinandro@outlook.com>
2021-02-05 10:59:44 +0530

    committed sum of 2 numbers

---
+++ src/com/MyTrial/javaexamples/SumOftwoNumbers.java
@@ -0,0 +1,10 @@
+package com.MyTrial.javaexamples;
+
+public class SumOftwoNumbers {
+    public static void main(String[] args){
+        int a = 7;
+        int b = 8;
+        int c = a+b;
+        System.out.println("the sum of two numbers is"+" "+c);
+    }
+}
