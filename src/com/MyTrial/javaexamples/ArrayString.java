8a9115555672cb25cfa82a8ba5a89f46563049dd
subin <subinandro@outlook.com>
2021-02-10 10:21:11 +0530

    all commited

---
+++ src/com/MyTrial/javaexamples/ArrayString.java
@@ -0,0 +1,11 @@
+package com.MyTrial.javaexamples;
+
+public class ArrayString {
+    public static void main(String[] args){
+        String[] texts = new String[3];
+        texts[0] = "hai,";
+        texts[1] = "hello";
+        texts[2] = "how are you";
+        System.out.println(texts[0]+" "+texts[1]+" "+texts[2]);
+    }
+}
---
+++ src/com/MyTrial/javaexamples/Array_demo.java
@@ -0,0 +1,8 @@
+package com.MyTrial.javaexamples;
+
+public class Array_demo {
+    public static void main(String[] args){
+        int[] number = {5,7,6,9,2,1,8};
+        System.out.println(number[2]+number[6]);
+    }
+}
