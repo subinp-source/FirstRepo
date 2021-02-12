b3024c2cfe952df67f4bd56a732fbc613516e9bf
subin <subinandro@outlook.com>
2021-02-12 11:23:16 +0530

    editted

--- src/com/MyTrial/javaexamples/LeapTask.java
+++ src/com/MyTrial/javaexamples/LeapTask.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class LeapTask {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        int n;
+        double n;
         System.out.println("enter the year");
-        n = s.nextInt();
-        if (n%400==0 && n%100==0){
-            System.out.println("it is a century leap year");}
-        else if (n%4==0){
-            System.out.println("it is a leap year");
-
-        }
-        else System.out.println("not a leap year");
-
+        int m=0;
+        n = s.nextDouble();
+       if (n%100==0){
+           if (n%400==0 && n%4==0){
+               System.out.println("century leap year");
+           }
+           else System.out.println("not a century leap year");
+       }
+       else if (n%4==0){
+           System.out.println("it is a leap year");
+       }
+       else System.out.println("not leap year");
     }
 }
